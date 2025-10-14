ca030ff3aa310744e23de3aec83b5acea01c0058
alinsarbu23 <sarbualin23@gmail.com>
2025-10-14 13:19:59 +0300

    Classes initialization
    Declaring the models, interfaces and the service class by default.

---
+++ NET-Mentee-Project/MiniBank/Models/Interfaces/ITransactable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBank.Models.Interfaces
+{
+    public interface ITransactable
+    {
+        void Deposit(decimal amount);
+        bool Withdraw(decimal amount, out string? error);
+    }
+}
