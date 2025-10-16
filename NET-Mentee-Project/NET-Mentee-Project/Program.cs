1d806c33f4f7af527f74a2f077ad04e95a05f4fe
alinsarbu23 <sarbualin23@gmail.com>
2025-10-16 17:26:15 +0300

    Solving the problem 1

--- NET-Mentee-Project/NET-Mentee-Project/Program.cs
+++ NET-Mentee-Project/NET-Mentee-Project/Program.cs
@@ -5,8 +5,33 @@ namespace NETMenteeProject
 {
     public class Program
     {
+        public static void SolvingProblem1()
+        {
+            int age;
+            string email;
+            bool isStudent;
+            string name;
+
+            Console.WriteLine("Name: ");
+            name = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Age:");
+            age = int.Parse(Console.ReadLine() ?? string.Empty);
+
+            Console.WriteLine("Email:");
+            email = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Are you a student? (yes/no)");
+            isStudent = (Console.ReadLine() ?? string.Empty).ToLower() == "yes";
+
+            Contract contract = new Contract(name, age, email, isStudent);
+            string result = contract.ToString();
+            Console.WriteLine(result);
+        }
+
         public static void Main(string[] args)
         {
+            
 
         }
     }
--- NET-Mentee-Project/NET-Mentee-Project/obj/Debug/net8.0/NET-Mentee-Project.AssemblyInfo.cs
+++ NET-Mentee-Project/NET-Mentee-Project/obj/Debug/net8.0/NET-Mentee-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NET-Mentee-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21c3e37febb23a7648176d4e8177213300527647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e72e4b7238af1711ced3404f9a1e32b9e3a79369")]
 [assembly: System.Reflection.AssemblyProductAttribute("NET-Mentee-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NET-Mentee-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
