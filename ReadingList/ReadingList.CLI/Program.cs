a138e35cf12f8744cd8a4fc6c11c9ebd23d834f3
alinsarbu23 <sarbualin23@gmail.com>
2025-11-07 16:06:14 +0200

    Adjustment
    1. Implement the CLI project
    2. Adjust the menu class in Program.cs
    3. Implement the Menu UI

--- ReadingList/ReadingList.CLI/Program.cs
+++ ReadingList/ReadingList.CLI/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ReadingList.CLI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+﻿using ReadingList.Application.Services;
+using ReadingList.CLI.Menus;
+using ReadingList.Infrastructure.FileServices;
+using ReadingList.Infrastructure.Repositories;
 
+namespace ReadingList.App
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var repo = new InMemoryBookRepository();
+            var bookService = new BookService(repo);
+            var importService = new ImportService(repo);
+            var exportService = new ExportService(repo);
+
+            var menu = new Menu(bookService, importService, exportService);
+            await menu.RunAsync();
         }
     }
 }
