9987845f43d52182b31a972ec0e92f52d1976f2c
alinsarbu23 <sarbualin23@gmail.com>
2025-12-24 16:34:53 +0200

    Implemment Auth bussiness logic
    1. Added Domain entity for user
    2. Creating the DTOs
    3. Added and implemmented IAuthManager interface and AuthManager repo
    4. Implemmented JwtTokenService for creating token
    5. Added JwtSettings options in appsettings.json

---
+++ AirportManagement/AirportTool.Application/Interfaces/IAuthManager.cs
@@ -0,0 +1,10 @@
+﻿using AirportTool.Infrastructure.DTOs.Auth;
+
+namespace AirportTool.Application.Interfaces
+{
+    public interface IAuthManager
+    {
+        Task<IEnumerable<string>> RegisterUserAsync(RegisterDto dto, CancellationToken ct);
+        Task<AuthResponseDto?> LoginAsync(LoginDto dto, CancellationToken ct);
+    }
+}
---
+++ AirportManagement/AirportTool.Domain/Models/ApiUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AirportTool.Infrastructure.Auth
+{
+    public class ApiUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
